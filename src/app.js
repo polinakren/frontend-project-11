73ea2dce32b71ba1e12fee3482e1568488d74c34
Polina Kren <polina.kren8952@mail.ru>
2023-12-13 00:28:15 +0200

    Task-3: refactoring eslint

--- src/app.js
+++ src/app.js
@@ -14,52 +14,52 @@ export default () => {
   const i18nextInstance = i18next.createInstance();
 
   i18nextInstance
-      .init({
-        lng: DEFAULT_LANGUAGE,
-        debug: false,
-        resources,
-      })
-      .then(() => {
-        setLocale({
-          mixed: {
-            default: i18nextInstance.t('validation.default'),
-            notOneOf: i18nextInstance.t('validation.notOneOf'),
-          },
-          string: {
-            required: i18nextInstance.t('validation.required'),
-            url: i18nextInstance.t('validation.invalidUrl'),
-          },
-        });
-
-        const watchedState = onChange(state, render(elements));
-
-        elements.formEl.addEventListener('submit', (e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target);
-          const url = formData.get('url');
-          validate(url, state.urls)
-              .then(() => {
-                watchedState.errors = '';
-                watchedState.status = 'valid';
-                watchedState.urls.push(url);
-              })
-              .catch((error) => {
-                watchedState.errors = error.message;
-                watchedState.status = 'invalid';
-              })
-              .then(() => console.log(state));
-        });
+    .init({
+      lng: DEFAULT_LANGUAGE,
+      debug: false,
+      resources,
+    })
+    .then(() => {
+      setLocale({
+        mixed: {
+          default: i18nextInstance.t('validation.default'),
+          notOneOf: i18nextInstance.t('validation.notOneOf'),
+        },
+        string: {
+          required: i18nextInstance.t('validation.required'),
+          url: i18nextInstance.t('validation.invalidUrl'),
+        },
       });
+
+      const watchedState = onChange(state, render(elements));
+
+      elements.formEl.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const url = formData.get('url');
+        validate(url, state.urls)
+          .then(() => {
+            watchedState.errors = '';
+            watchedState.status = 'valid';
+            watchedState.urls.push(url);
+          })
+          .catch((error) => {
+            watchedState.errors = error.message;
+            watchedState.status = 'invalid';
+          })
+          .then(() => console.log(state));
+      });
+    });
 };
 
 const getElements = () => ({
-    formEl: document.querySelector('.rss-form'),
-    inputEl: document.querySelector('#url-input'),
-    feedbackEl: document.querySelector('.feedback'),
+  formEl: document.querySelector('.rss-form'),
+  inputEl: document.querySelector('#url-input'),
+  feedbackEl: document.querySelector('.feedback'),
 });
 
 const state = {
-    status: '',
-    errors: '',
-    urls: [],
+  status: '',
+  errors: '',
+  urls: [],
 };
